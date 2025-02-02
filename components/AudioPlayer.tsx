f941aaf5f3afb8df83c97cffa1c711b4b59bc520
yasaswini <yasaswiniv@keysoftwareinc.com>
2025-02-02 13:23:34 +0530

    first deploy

---
+++ components/AudioPlayer.tsx
@@ -0,0 +1,66 @@
+"use client"
+import { useEffect, useRef, useState } from "react";
+
+const AudioPlayer = () => {
+  const audioRef1 = useRef<HTMLAudioElement | null>(null);
+  const audioRef2 = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  // Function to start playing audio
+  const handleMouseMove = () => {
+    console.log("mouse moved");
+    const audio1 = audioRef1.current;
+    const audio2 = audioRef2.current;
+
+    if (audio1 && audio2 && !isPlaying) {
+      console.log("in");
+      audio1.muted = true; // Mute initially to bypass autoplay restrictions
+
+      audio1.play()
+        .then(() => {
+          setIsPlaying(true);
+          audio1.muted = false; // Unmute after the first play attempt
+        })
+        .catch((err) => {
+          //console.error("Error while trying to play audio:", err);
+        });
+
+      // Set up the loop behavior between two tracks
+      audio1.onended = () => {
+        audio2.play();
+      };
+
+      audio2.onended = () => {
+        audio1.play();
+      };
+    }
+  };
+
+  
+
+  useEffect(() => {
+    // Attach mousemove event listener when the component is mounted
+    document.addEventListener("click", handleMouseMove);
+    
+
+    // Cleanup the event listener on unmount
+    return () => {
+      document.removeEventListener("click", handleMouseMove);
+    };
+  }, [isPlaying]);
+
+  return (
+    <div style={{ display: "none" }}>
+      <audio ref={audioRef1} loop>
+        <source src="/songs/bgm1.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+      <audio ref={audioRef2} loop>
+        <source src="/songs/bgm2.mp3" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
+};
+
+export default AudioPlayer;
