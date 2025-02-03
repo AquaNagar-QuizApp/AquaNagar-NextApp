9feb98ed0a977fa9986811c5138e7344e0ba35eb
yasaswini <yasaswiniv@keysoftwareinc.com>
2025-02-03 09:52:30 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:"export",
-  images: {
-    unoptimized: true,
-  }
+  output:"export"
 };
 
 export default nextConfig;
