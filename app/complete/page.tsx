f044cfdd11e0dd5ef07d014e80f5763dc0fce4af
Arunbalaji Singaravelu <ArunS@keysoftwareinc.com>
2025-03-20 11:52:14 +0530

    Removed old code block from complete page

--- app/complete/page.tsx
+++ app/complete/page.tsx
@@ -318,38 +318,7 @@ function StageScoreSection({ router, isMuted, backgroundAudioSrc, playBackground
   const handleDownloadAnswerSheet = () => {
     generateAnswerSheet(set);
   }
-
-  // return (
-  //   <div className="flex flex-col items-center justify-center min-h-screen">
-  //     {/* <audio ref={winningSoundRef} src="./soundeffects/winningsound.mp3" /> */}
-  //     {score > 0 && <Confetti width={windowSize.width} height={windowSize.height} />}
-
-  //     <h1
-  //       className={`text-4xl sm:text-6xl font-bold mb-8 text-center 
-  //       ${score > 0 ? "text-white animate-bounce" : "text-yellow-300"}`}
-  //     >
-  //       {score > 0 ? "Congratulations!" : "Don't Give Up!"}
-  //     </h1>
-
-  //     <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-lg p-8 mb-8 text-center">
-  //       <p className="text-3xl text-white mb-4">
-  //         {score > 0 ? "You have successfully completed" : "You have attempted"}
-  //       </p>
-  //       <p className="text-4xl text-white mb-4">
-  //         <strong>{stage} Stage</strong>
-  //       </p>
-  //       <p className="text-3xl text-white mb-4">{score > 0 ? "with a score of" : "but didn't score any points."}</p>
-  //       {score > 0 && <p className={"text-6xl font-bold animate-pulse text-blue-800"}>{score}</p>}
-  //     </div>
-
-  //     <button
-  //       className="px-6 py-2 bg-blue-700 text-white rounded-lg font-semibold backdrop-blur-lg text-lg transition duration-300 ease-in-out transform hover:bg-blue-800 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
-  //       onClick={handleReturnToStages}
-  //     >
-  //       Return to Stages Page
-  //     </button>
-  //   </div>
-  // );
+  
   const showConfetti = (allSectionsCompleted && totalScore > 0) || (!allSectionsCompleted && score > 0);
 
   return (
